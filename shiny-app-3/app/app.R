03140bc1972547c0255f1073c621099ad73f1b1c
kzarada <katherinezarada@mac.lan>
2026-07-29 08:54:11 -0400

    updating file path

--- shiny-app-3/app/app.R
+++ shiny-app-3/app/app.R
@@ -19,8 +19,7 @@ library(htmlwidgets)
 library(plotly)
 
 #Set Data File Path (changes for dockerfile)
-#data_dir = "/srv/shiny-server/Data/"
-data_dir = "~/Documents/01_Data_Products/Double_Shiny/Data/"
+data_dir = "/srv/shiny-server/Data/"
 ################## Read in data #####################
 instrument.locations = read.csv(file.path(data_dir, "Inputs/RealTimeMonitoring_Locations.csv")) %>% 
   dplyr::select(Name, ID, Latitude, Longitude) 
